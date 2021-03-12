5f446e9b7369ecbdd13778488f5bec6e1901ff4c
Jordan Jarvis <jordan.jarvis@securitymetrics.com>
2021-03-12 08:21:06 -0700

    Added description

--- BarrierAuto.sh
+++ BarrierAuto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script opens Barrier (mouse and keyboard sharing software) on the host machine. It will ssh to the ipv6 address as it is basically static, get the ipv4 address, and start the client connecting to the server with the ipv4 address. I did this to fix changing my IP manually any time the host IP changed. 
 barrier &
 IPADDR=$(ip addr show enp4s0)
 IPADDR2="`echo "$IPADDR" | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`"
